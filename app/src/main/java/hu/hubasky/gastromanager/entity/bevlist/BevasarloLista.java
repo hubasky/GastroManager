e064cbc7b6db8b17001a729ba7777ce2f2df5e72
Makra Attila <mirsoft.bt@upcmail.hu>
2017-04-26 16:39:26 +0200

    A felhasználókezelés entitásai és vezérlője.

---
+++ app/src/main/java/hu/hubasky/gastromanager/entity/bevlist/BevasarloLista.java
@@ -0,0 +1,8 @@
+package hu.hubasky.gastromanager.entity.bevlist;
+
+/**
+ * Created by mirso on 2017. 04. 26..
+ */
+
+public class BevasarloLista {
+}
---
+++ app/src/main/java/hu/hubasky/gastromanager/entity/recept/Recept.java
@@ -0,0 +1,8 @@
+package hu.hubasky.gastromanager.entity.recept;
+
+/**
+ * Created by mirso on 2017. 04. 26..
+ */
+
+public class Recept {
+}
