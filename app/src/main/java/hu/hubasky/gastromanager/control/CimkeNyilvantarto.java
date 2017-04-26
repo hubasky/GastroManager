391c1911cd5b30f6381b3a43501e8fba6d6f65c5
Makra Attila <mirsoft.bt@upcmail.hu>
2017-04-26 16:18:23 +0200

    A cimkék vezérlője.

---
+++ app/src/main/java/hu/hubasky/gastromanager/control/CimkeNyilvantarto.java
@@ -0,0 +1,29 @@
+package hu.hubasky.gastromanager.control;
+
+import java.util.List;
+
+import hu.hubasky.gastromanager.entity.Cimke;
+import hu.hubasky.gastromanager.entity.ECimkeTipus;
+
+/**
+ * A cimkék nyilvántartója.
+ * Created by mirso on 2017. 04. 26..
+ */
+
+public interface CimkeNyilvantarto {
+    /**
+     * Cimkék keresése.
+     * @param tipus a cimke típus.
+     * @param nevtoredek a cimkében szereplő névtöredék.
+     * @return a találatok.
+     * @throws Exception Ha kivétel lépett fel.
+     */
+    List<Cimke> keres(ECimkeTipus tipus, String nevtoredek) throws Exception;
+
+    /**
+     * Cimke tárolása.
+     * @param cimke a cimke példány.
+     * @throws Exception Ha kivétel lépett fel.
+     */
+    void tarolas(Cimke cimke) throws Exception;
+}
