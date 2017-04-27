9b051554e597096ee2e5fbbd91ca23600876ef97
HP <hpeet3d@gmail.com>
2017-04-27 04:55:34 +0200

    reciepeManager UI

--- app/src/main/java/hu/hubasky/gastromanager/ReciepeManagerActivity.java
+++ app/src/main/java/hu/hubasky/gastromanager/ReciepeManagerActivity.java
@@ -2,6 +2,14 @@ package hu.hubasky.gastromanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import hu.hubasky.gastromanager.viewmodel.IngredientVM;
+import hu.hubasky.gastromanager.viewmodel.ReciepeAdapter;
+import hu.hubasky.gastromanager.viewmodel.ReciepeVM;
 
 public class ReciepeManagerActivity extends AppCompatActivity {
 
@@ -9,5 +17,18 @@ public class ReciepeManagerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reciepe_manager);
+
+        List<ReciepeVM> reciepesList = new ArrayList<ReciepeVM>();
+        reciepesList.add(new ReciepeVM("Recept 1", "Leírás 1", new ArrayList<IngredientVM>()));
+        reciepesList.add(new ReciepeVM("Recept 2", "Leírás 2", new ArrayList<IngredientVM>()));
+        reciepesList.add(new ReciepeVM("Recept 3", "Leírás 3", new ArrayList<IngredientVM>()));
+        reciepesList.add(new ReciepeVM("Recept 4", "Leírás 4", new ArrayList<IngredientVM>()));
+        reciepesList.add(new ReciepeVM("Recept 5", "Leírás 5", new ArrayList<IngredientVM>()));
+
+        final ReciepeAdapter reciepeAdapter = new ReciepeAdapter(reciepesList);
+        ListView reciepeListView = (ListView) findViewById(R.id.reciepe_list);
+
+//        reciepeListView.setAdapter(reciepeAdapter);
+
     }
 }
