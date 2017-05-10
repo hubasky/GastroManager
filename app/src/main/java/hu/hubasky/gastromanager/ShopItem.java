0dd070784fdafe086ec5672be4f1ce7616664187
Balázs <Jeghideg@gmail.com>
2017-05-11 00:04:25 +0200

    ShoppingListActivity hozzáadva, UI rész működik

---
+++ app/src/main/java/hu/hubasky/gastromanager/ShopItem.java
@@ -0,0 +1,48 @@
+package hu.hubasky.gastromanager;
+
+class ShopItem {
+
+    private String name;
+    private int quantity;
+    private String quantityType;
+    private boolean selected = false;
+
+    public ShopItem(String name, int quantity, String quantityType) {
+        super();
+        this.name = name;
+        this.quantity = quantity;
+        this.quantityType = quantityType;
+    }
+
+    public String getQuantityType() {
+        return quantityType;
+    }
+
+    private void setQuantityType(String quantityType) {
+        this.quantityType = quantityType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+}
